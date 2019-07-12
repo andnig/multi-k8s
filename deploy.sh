7a8305d0489fe68704a3fb65127d73bc2fc7a4d9
Andreas Nigg <andreas.nigg@senseforce.io>
2019-07-12 17:41:33 +0200

    fixed travis deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t andnig/multi-client:latest -t andnig/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t andnig/multi-server:latest -t andnig/multi-server:$SHA -f ./client/Dockerfile ./server
-docker build -t andnig/multi-worker:latest -t andnig/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t andnig/multi-server:latest -t andnig/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t andnig/multi-worker:latest -t andnig/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push andnig/multi-client:latest
 docker push andnig/multi-server:latest
 docker push andnig/multi-worker:latest
